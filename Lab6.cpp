b1ac1aaace9532c4e0f5e5c6a918b3ef29c3ca0a
teacher <teacher@mail.ua>
2014-10-29 22:43:04 +0200

    add check for dividig by zero

--- Lab6.cpp
+++ Lab6.cpp
@@ -5,6 +5,11 @@ void main()
 	double x, y;
 	cout << "X=";
 	cin >> x;
+	if(fabs(x - 20) < 0.0001)
+	{
+		cout << "Error! divide by zero";
+		return;
+	}
 	y = (x + 10) / (x - 20);
 	cout << "y = " << y;
 }
